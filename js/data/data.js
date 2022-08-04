11402990c254a0cbbf8d00c601aea8544ce426b9
RGN-QvQ <62834219+QvQ-Code@users.noreply.github.com>
2022-08-04 23:55:40 +0700

    Add files via upload

---
+++ js/data/data.js
@@ -0,0 +1,119 @@
+var data = {
+    "name" : "valentz",
+    "illustration" : [
+        {
+            "id"    : "1",
+            "url"   : "https://pbs.twimg.com/media/FS9YkI7UAAAoVul?format=jpg&name=4096x4096",
+            "source"   : "https://twitter.com/lemon_mito/status/1526536734363688960",
+            "platform": "Twitter",
+            "artist": "Mito Nagishiro"
+        },
+        {
+            "id"    : "2",
+            "url"   : "https://64.media.tumblr.com/50427808c4927f31742d0171cdb19fbc/b8ed34ff66e444a0-f2/s1280x1920/63fa2c37f7f7c85566da6f1e1ff7a5c817888876.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/658083137404944384/%E7%B7%B4%E7%BF%92",
+            "platform": "Tumblr",
+            "artist": "Mito Nagishiro"
+        },
+        {
+            "id"    : "3",
+            "url"   : "https://64.media.tumblr.com/91f915626e6077c395edd9c83f6204bb/tumblr_pg4m2yX8Oe1uvodoho1_1280.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/178757430145/happy-birthday",
+            "platform": "Tumblr",
+            "artist": "Mito Nagishiro"
+        },
+        {
+            "id"    : "4",
+            "url"   : "https://64.media.tumblr.com/9cfc3228f172eee5483bf30fc7ddc3d7/tumblr_ou0cynjSi61uvodoho1_1280.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/163675674975/%E5%A4%8F%E3%82%B3%E3%83%9F%E3%81%AE%E7%AD%89%E8%BA%AB%E5%A4%A7%E3%82%BF%E3%83%9A%E3%82%B9%E3%83%88%E3%83%AA%E3%83%BC%E7%94%A8%E3%81%AB%E6%8F%8F%E3%81%8D%E3%81%BE%E3%81%97%E3%81%9F",
+            "platform": "Tumblr",
+            "artist": "Mito Nagishiro"
+        },
+        {
+            "id"    : "5",
+            "url"   : "https://64.media.tumblr.com/7037e7d7c86d4fb517a2670a034fabcb/f1ecfb89c31b34fd-44/s1280x1920/7cbf162496ac11f3b0bc1843ebde61723ffcead3.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/649434137766264832/%E4%BB%8A%E5%B9%B4%E3%82%82%E3%82%88%E3%82%8D%E3%81%97%E3%81%8F%E3%81%8A%E9%A1%98%E3%81%84%E3%81%97%E3%81%BE%E3%81%99",
+            "platform": "Tumblr",
+            "artist": "Mito Nagishiro"
+        },
+        {
+            "id"    : "6",
+            "url"   : "https://pbs.twimg.com/media/FZN2E2OUIAA2cNj?format=jpg&name=4096x4096",
+            "source"   : "https://twitter.com/Purizm_edit/status/1554738843433127936/photo/1",
+            "platform": "Twitter",
+            "artist": "Rurudo"
+        },
+        {
+            "id"    : "7",
+            "url"   : "https://pbs.twimg.com/media/FZS7UUaVEAA0mcY?format=jpg&name=large",
+            "source"   : "https://mobile.twitter.com/AC______/status/1555074159729815552/photo/1",
+            "platform": "Twitter",
+            "artist": '泉彩　1日目西地区 "め "10b'
+        },
+        {
+            "id"    : "8",
+            "url"   : "https://pbs.twimg.com/media/Do5CbaFVAAAqZVp?format=jpg&name=large",
+            "source"   : "https://twitter.com/utsugame/status/1048850185982423041/photo/1",
+            "platform": "Twitter",
+            "artist": 'Utsugame'
+        },
+        {
+            "id"    : "9",
+            "url"   : "https://64.media.tumblr.com/31dbdaf941e5b4c6203da6e3f5bd51a5/49e108af13e73c64-65/s1280x1920/3611ebbe6c136f54a4d8dc69485c41a28d25f539.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/615628204171051008/%EF%BD%92%EF%BD%8B%EF%BD%87%EF%BD%8B",
+            "platform": "Tumblr",
+            "artist": 'Mito Nagishiro'
+        },
+        {
+            "id"    : "10",
+            "url"   : "https://64.media.tumblr.com/cd1bba14f61e30b0cffb2faa01a798b8/5b6db62ffff0528f-73/s1280x1920/9d6cbac686796e4ade3d3954d8e22923d0782075.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/652048045336395776",
+            "platform": "Tumblr",
+            "artist": 'Mito Nagishiro'
+        },
+        {
+            "id"    : "11",
+            "url"   : "https://64.media.tumblr.com/8b4509a420badd58b4fd613abc41753c/9b2ac7ae6c037650-64/s1280x1920/b075ffe939fe6167d37b957028d696c974fee848.jpg",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/634401232121921536/ai-vtuber%E3%81%AE%E7%B4%A1%E3%83%8D%E3%83%B3%E3%81%AE%E3%82%AD%E3%83%A3%E3%83%A9%E3%82%AF%E3%82%BF%E3%83%BC%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3%E3%82%92%E3%81%95%E3%81%9B%E3%81%A6%E3%81%84%E3%81%9F%E3%81%A0%E3%81%8D%E3%81%BE%E3%81%97%E3%81%9F",
+            "platform": "Tumblr",
+            "artist": 'Mito Nagishiro'
+        },
+        {
+            "id"    : "12",
+            "url"   : "https://64.media.tumblr.com/13b6549c28296706c4a426ff5ce239c5/5d2cecf3a6df09db-f3/s1280x1920/711165ce8328a52c3b9cf2d77e315720710713c3.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/622859752686452736",
+            "platform": "Tumblr",
+            "artist": 'Mito Nagishiro'
+        },
+        {
+            "id"    : "13",
+            "url"   : "https://64.media.tumblr.com/571634da2ef3fb6e0c0f84b7982c5025/5d2cecf3a6df09db-f0/s1280x1920/7a19c483d7a3a797a2bde54c65dc56464a688211.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/622859752686452736",
+            "platform": "Tumblr",
+            "artist": 'Mito Nagishiro'
+        },
+        {
+            "id"    : "14",
+            "url"   : "https://64.media.tumblr.com/493efd25879659c47cd96a65668397e5/6915ef1f003472da-fb/s1280x1920/2a6d72eebe928190604ee7b8d29dfa71e90a7e26.png",
+            "source"   : "https://mito-nagishiro.tumblr.com/post/615628291892805632/%EF%BD%92%EF%BD%8B%EF%BD%87%EF%BD%8B",
+            "platform": "Tumblr",
+            "artist": 'Mito Nagishiro'
+        },
+
+    ]
+}
+
+var illust_length = data.illustration.length;
+var illust = data.illustration;
+
+function illustration(a) { 
+    document.write("<img src='"+ illust[a].url +"''>");
+}
+
+function illustration_artist(a){
+    document.write(illust[a].artist);
+}
+
+function illustration_link(a) { 
+    document.write("<a href='"+ illust[a].source +"' target='_blank'>" + illust[a].platform + "</a>");
+}
